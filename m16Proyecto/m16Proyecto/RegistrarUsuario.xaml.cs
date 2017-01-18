8a680ca093fb99162cc06c1a116100cb14b6535b
Alexis <arodriguezsilvente@gmail.com>
2017-01-18 15:55:13 +0100

    Creando clases del modulo de Usuarios

---
+++ m16Proyecto/m16Proyecto/RegistrarUsuario.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace m16Proyecto
+{
+    /// <summary>
+    /// Interaction logic for RegistrarUsuario.xaml
+    /// </summary>
+    public partial class RegistrarUsuario : Window
+    {
+        public RegistrarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void botonSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
