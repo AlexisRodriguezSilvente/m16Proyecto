3b970f03837290ca39ec039b8f16974fd5b9b89c
Alexis <arodriguezsilvente@gmail.com>
2017-01-21 00:14:32 +0100

    creada carpeta mysql y clase conexion

---
+++ m16Proyecto/m16Proyecto/mysql/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace m16Proyecto.mysql
+{
+    class Conexion
+    {
+        public static MySqlConnection obtenerConexion()
+        {
+            MySqlConnection Conexion = new MySqlConnection("server=127.0.0.1;database=pruebalogin;Uid=root;pwd=4fr0dt4.");
+            Conexion.Open();
+            return Conexion;
+        }
+    }
+}
