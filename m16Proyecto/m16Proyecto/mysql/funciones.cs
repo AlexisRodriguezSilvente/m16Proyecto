680f53dee9870d006dd7de6ba5fcb484a1bb44e0
Alexis <arodriguezsilvente@gmail.com>
2017-01-21 02:39:33 +0100

    AÑADIDO BOTON AGREGAR

---
+++ m16Proyecto/m16Proyecto/mysql/funciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace m16Proyecto.mysql
+{
+    public class funciones
+    {
+        public static int agregar(usuario add)
+        {
+            int retorno = 0;
+            MySqlCommand comando = new MySqlCommand(String.Format("insert into usuarios(idUsuario, nombre, primerApellido, segundoApellido, dni, correo , telefono, direccion, ciudad, contraseña , categoria)values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}')"
+                ,add.idUsuario,add.nombre,add.primerApellido,add.segundoApellido,
+                add.dni,add.correo,add.telefono,add.direccion,add.ciudad,add.contraseña,add.categoria));
+            retorno = comando.ExecuteNonQuery();
+            return retorno;
+        }
+    }
+}
---
+++ m16Proyecto/m16Proyecto/mysql/usuario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m16Proyecto.mysql
+{
+    public class usuario
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string primerApellido { get; set; }
+        public string segundoApellido { get; set; }
+        public string dni { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+        public string ciudad { get; set; }
+        public string contraseña { get; set; }
+        public int categoria { get; set; }
+        public usuario() { }
+
+        public usuario(int idUsuario, string nombre, string primerApellido, string segundoApellido,
+                       string dni, string correo, string telefono, string direccion,
+                       string ciudad, string contraseña, int categoria)
+        {
+            this.idUsuario = idUsuario;
+            this.nombre = nombre;
+            this.primerApellido = primerApellido;
+            this.segundoApellido = segundoApellido;
+            this.dni = dni;
+            this.correo = correo;
+            this.telefono = telefono;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.contraseña = contraseña;
+            this.categoria = categoria;
+
+        }
+    }
+}
